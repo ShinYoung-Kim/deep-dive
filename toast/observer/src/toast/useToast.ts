0bdc528c2d873f8182ab061cde5b60b012980b3f
ShinYoung-Kim <lukey0515@naver.com>
2024-11-08 11:25:41 +0900

    fix: observer 패턴 방식 toast 추가 버그 수정

--- toast/observer/src/toast/useToast.ts
+++ toast/observer/src/toast/useToast.ts
@@ -4,7 +4,13 @@ import { ToastList } from "./toast.type";
 export const useToast = () => {
 	const [toastList, setToastList] = useState<ToastList>([]);
 	const addToast = (message: string) => {
-		setToastList([...toastList, { message, id: Date.now() }]);
+		setToastList((prev) => [
+			...prev,
+			{
+				id: Date.now(),
+				message,
+			},
+		]);
 	};
 	const removeToast = (id: number) => {
 		setToastList(toastList.filter((toast) => toast.id !== id));
