53f6e4c37810d47f7d5a7c49deb8579a6edc1738
ShinYoung-Kim <lukey0515@naver.com>
2024-11-15 17:02:18 +0900

    refactor: context api 방식 불필요한 재렌더링 제거

--- toast/src/contextAPI/ToastProvider.tsx
+++ toast/src/contextAPI/ToastProvider.tsx
@@ -5,10 +5,10 @@ import { ToastContext } from "./ToastContext";
 export const ToastProvider = ({ children }: PropsWithChildren) => {
 	const [toastList, setToastList] = useState<ToastList>([]);
 	const addToast = (message: Message) => {
-		setToastList([...toastList, { message, id: Date.now() }]);
+		setToastList((prev) => [...prev, { message, id: Date.now() }]);
 	};
 	const removeToast = (id: number) => {
-		setToastList(toastList.filter((toast) => toast.id !== id));
+		setToastList((prev) => prev.filter((toast) => toast.id !== id));
 	};
 	return <ToastContext.Provider value={{ toastList, addToast, removeToast }}>{children}</ToastContext.Provider>;
 };
